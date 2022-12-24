287b6547bd7f87e2b6f5e564bb75739580dd9849
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2022-12-24 18:50:33 +0300

    added base structure

---
+++ src/js/classroom.js
@@ -0,0 +1,44 @@
+/* classroom.js */
+
+/** @param {number[]} grades */
+export function getNumberOfGrades(grades) {
+	return grades.length
+}
+
+/** @param {number[]} grades */
+export function getSumGrades(grades) {
+    const sum = grades.reduce(function (total, current) {
+        return total + current;
+      }, 0);
+      return sum;
+}
+
+/** @param {number[]} grades */
+export function getAverageValue(grades) {
+    return getSumGrades(grades)/grades.length
+}
+
+/** @param {number[]} grades */
+export function getPassingGrades(grades) {
+    const passedGrades = grades.filter(function(grade) {
+        return grade > 10;
+    });
+    return passedGrades.join(', ')
+}
+
+/** @param {number[]} grades */
+export function getFailingGrades(grades) {
+    const faledGrades = grades.filter(function(grade) {
+        return grade < 10;
+    });
+    return faledGrades.join(', ')
+}
+
+/** @param {number[]} grades */
+export function getRaisedGrades(grades) {
+    const raisedGrades = grades.map(function(grade) {
+        return grade + 1 ;
+    });
+    return raisedGrades.join(', ')
+
+}
